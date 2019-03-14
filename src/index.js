964fca65a843cb850410f6a351e49ea3ba08eefc
sazar84 <45146694+sazar84@users.noreply.github.com>
2019-03-15 01:28:40 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,40 +1,69 @@
 module.exports = function solveSudoku(matrix) {
+
   let sudoku = [];
   sudoku = matrix;
 
-  let row = []; 
+  let row = [];
   let col = [];
+  let box = [];
 
-  for (var i = 0; i < 9; i++) {
-   count: for (var j = 0; j < 9; j++) {
-      var zero = sudoku[i][j];
-      if (zero != 0) {
+  for (let i = 0; i < 9; i++) {
+    count: for (let j = 0; j < 9; j++) {
+      var iszero = sudoku[i][j];
+      if (iszero != 0) {
         continue;
       } else {
 
-          for (var z = 0; z < 9; z++) {
-                row.push(sudoku[i][z]);
-                // console.log(row)
+        for (var z = 0; z < 9; z++) {
+          row.push(sudoku[i][z]);
+        }
+        // console.log(row);
+
+        for (var x = 0; x < 9; x++) {
+          col.push(sudoku[x][j]);
+        }
+        // console.log(col)
+
+        if (i < 3 && j < 3) {
+          for (var m = 0; m < 3; m++) {
+            for (var k = 0; k < 3; k++) {
+              box = sudoku.slice([m][k], 1)
+            }
           }
-             for (var x = 0; x < 9; x++) {
-               col.push(sudoku[x][j]);
-               // console.log(col)
-             }
-                for (var c = 1; c <= 9; c++) {
-                  if (!row.includes(c) && !col.includes(c)) {
-                    sudoku[i][j] = c;
-                    // console.log(c)
-                    row = [];
-                    col = [];
-                    continue count;
-                  }
-               // return sudoku
-                }
-                 return sudoku
+        }
+        // console.log(box)
+
+        if (6 > i && i >= 3 && j < 3) {
+          for (var m = 0; m < 3; m++) {
+            for (var k = 0; k < 3; k++) {
+              box = sudoku.slice([3 + m][k], 1)
+            }
+          }
+        }
+        // console.log(box)
+
+        if (i >= 6 && i < 9 && j < 3) {
+          for (var m = 0; m < 3; m++) {
+            for (var k = 0; k < 3; k++) {
+              box = sudoku.slice([6 + m][k], 1)
+            }
+          }
+        }
+        // console.log(box)
+
+
+        for (var c = 1; c <= 9; c++) {
+          if (!row.includes(c) && !col.includes(c) && !box.includes(c)) {
+            sudoku[i][j] = c;
+            // console.log(c)
+            row = [];
+            col = [];
+            box = [];
+            continue count;
+          }
+        }
       }
-      return sudoku
     }
-    return sudoku
   }
- return sudoku
+  return sudoku
 }
